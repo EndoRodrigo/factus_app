939d88ec4a193f0d66ef66090ee51b5dcc03ac43
EndoRodrigo <endorodrigo19@gmail.com>
2026-09-08 21:26:01 -0500

    fix: se actualiza nombre de la constante auth

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,4 +1,4 @@
 class ApiConstants {
   static const String baseUrl = 'https://api-sandbox.factus.com.co';
-  static const String authEnpoint = '/oauth/token';
+  static const String authEndpoint = '/oauth/token';
 }
