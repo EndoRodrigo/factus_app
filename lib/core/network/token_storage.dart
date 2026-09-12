6d540887e1f54a6a3f3e6a0861dbc0c0377b2dbc
EndoRodrigo <endorodrigo19@gmail.com>
2026-09-11 19:43:51 -0500

    Feat: se crea la clase para almacenar en storage token y refrest token

---
+++ lib/core/network/token_storage.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class TokenStorage {
+  static const _accessTokenKey = 'access_token';
+  static const _refreshTokenKey = 'refresh_token';
+
+  final FlutterSecureStorage storage;
+
+  const TokenStorage({this.storage = const FlutterSecureStorage()});
+
+  Future<void> saveTokens({
+    required String accessToken,
+    required String refreshToken,
+  }) async {
+    await storage.write(key: _accessTokenKey, value: accessToken);
+
+    await storage.write(key: _refreshTokenKey, value: refreshToken);
+  }
+
+  Future<String?> getAccessToken() async {
+    return storage.read(key: _accessTokenKey);
+  }
+
+  Future<String?> getRefreshToken() async {
+    return storage.read(key: _refreshTokenKey);
+  }
+
+  Future<void> clearTokens() async {
+    await storage.delete(key: _accessTokenKey);
+
+    await storage.delete(key: _refreshTokenKey);
+  }
+}
